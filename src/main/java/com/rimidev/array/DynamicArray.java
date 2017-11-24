04d0ca0860886026bff208fbed8eee9523a82e1a
Maxime Lacasse <maximelacasse@Maximes-MacBook-Pro.local>
2017-11-23 19:10:24 -0500

    Dynamic Array is complete with the unit tests! Totally functional.

--- src/main/java/com/rimidev/array/DynamicArray.java
+++ src/main/java/com/rimidev/array/DynamicArray.java
@@ -1,14 +1,24 @@
 package com.rimidev.array;
 
+import java.util.Arrays;
+import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author maximelacasse
  * @version 1.0
  */
-public class DynamicArray {
+public class DynamicArray <T>{
+    
+    //Logger for debugging.
+    private final Logger log = LoggerFactory.getLogger(
+            this.getClass().getName());
     
     private Object[] container; //The actual array to manipulate (The whole size of the array)
     private int size = 0; //The size of the array (Where they are some objects inside)
+    
     
     /**
      * Default constructor which creates the container array with 10 slots.
@@ -28,8 +38,15 @@ public class DynamicArray {
     /**
      * Adds the obj to the end of the array.
      */
-    public void add(Object obj){
-        this.container[size+1] = obj;
+    public void add(T obj){
+        if (size == this.container.length){
+            log.debug("Hit the copy: " + (this.container.length - size+1));
+            //If we add this to the end, then the container will be maximized, need to double
+            //the size before proceeding.
+            copy(this.container.length*2); //Copies the array to a new array with doubled size.
+        }
+        log.debug("new size: " + size);
+        this.container[size] = obj;
         size++;
     }
     
@@ -40,12 +57,20 @@ public class DynamicArray {
      * for the obj, place it.
      * Increase the size in either cases.
      * @param index 
-     * @param obj 
+     * @param t 
      */
-    public void add(int index, Object obj) {
+    public void add(int index, T t) {
+        if (index > size){
+            //Cannot add a value that is at an index greater than the size.
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
+        //If the array at position 0 is null, user has to use the add() method.
+        if (this.container[0] == null){
+           throw new NullPointerException("Please use the add() method for first inserts");
+        }
         //If the array at position:index is empty, add the object to the index.
         if (this.container[index] == null){
-           this.container[index] = obj; //Adds the objects to the desired index position.
+           this.container[index] = t; //Adds the objects to the desired index position.
            size++; //Increase the size.
         } else {
         /**
@@ -58,13 +83,13 @@ public class DynamicArray {
         if (this.container.length - size+1 == 0){
             //1. The array will be maximized if we add this object to the desired index.
             //Create a new array with double the size.
-           copy(container,container.length*2); //Copies the array to a new array with doubled size.
+           copy(this.container.length*2); //Copies the array to a new array with doubled size.
         }
         //2. Shift all the proceeding objects from index +1 positions.
         shift(index,1);
         
         //3. Add the object to the desired index position.
-        this.container[index] = obj; 
+        this.container[index] = t; 
         size++; //Increase the size.
         }       
     } // End of add method.
@@ -75,6 +100,10 @@ public class DynamicArray {
      * @return 
      */
     public Object get(int index) {
+        if (index > size){
+            //Cannot add a value that is at an index greater than the size.
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
      return this.container[index];
     }
     /**
@@ -82,10 +111,10 @@ public class DynamicArray {
      * @param anObject
      * @return 
      */
-    public int indexOf(Object anObject) {
+    public int indexOf(T t) {
         //Iterate through the whole array to check if any of the objects are equal to the anObject.
-        for (int i = 0; i < this.container.length; i++){
-            if (this.container[i].equals(anObject)){
+        for (int i = 0; i <= this.container.length; i++){
+            if (this.container[i] == t){
                 //The object at 'i' is equavalent to anObject, returning index.
                 return i;
             }
@@ -94,13 +123,23 @@ public class DynamicArray {
     }
 
     /**
-     * 
+     * Remove the object at the end of the container.
+     * Decrease the size variable.
+     */   
+    public void remove (){
+        shift(--size,-1);
+    }
+    
+    /**
+     * Remove the object at the index position.
+     * Decrease the size variable.
      * @param index 
      */
     public void remove (int index) {
-        shift(index,1); //Recreate the container array to not have the object in the index position.
+        shift(index,-1); //Recreate the container array to not have the object in the index position.
         size--; //Decrease the size.
     }
+
     /**
      * Returns how many positions are used up by objects in the array.
      * This does not return the total containing size of the array.
@@ -109,23 +148,54 @@ public class DynamicArray {
     public int size() {
      return size;
     }
-
-    public Object set(int index, Object obj){
-    //TO DO
-    return null;
-  }
+    /**
+     * Change the value at the specific index position with the obj.
+     * @param index The index that needs to change obj
+     * @param t The obj that will replace the object in position index
+     */
+    public void set(int index, T t){
+        if (index < size){
+            //Switch the value
+            
+            //Create a temporary array.
+            Object[] tempArray = this.container;
+            //Grab the capacity of the array.
+            int capacity = this.container.length;
+            //Re-create the container.
+            this.container = new Object[capacity];
+            //Iterate through the whole array, when hit the index position
+            //Add the obj at the position rather than the tempArray object.
+            for (int i = 0; i < capacity; i++){
+                if (index == i){
+                    //Add the obj at the index.
+                    this.container[i] = t;
+                } else {
+                    //Add the objects from the previous container to same indexes.
+                    this.container[i] = tempArray[i];
+                }                    
+            }
+            
+        } else {
+            //Cannot switch value that is higher than the size
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
+    }
     /**
      * Copies the array passed to the method into the DynamicArray object array
      * @param pastingArray The array that we want to copy into the container.
      * @param capacity The size of the desired container.
      */
-    public void copy(Object[] pastingArray, int capacity){
-        Object[] tempArray = pastingArray;
+    private void copy(int capacity){
+        Object[] tempArray = this.container;
+        log.debug("Capacity: " + capacity);
         this.container = new Object[capacity];
+        log.debug("new array length: " + this.container.length);
         
-        for (int i = 0; i < capacity; i++){
+        for (int i = 0; i < size; i++){
             this.container[i] = tempArray[i];
+            log.debug("new array: " + this.container[i]);
         }
+        log.debug("end of copy");
         
     }
     /**
@@ -134,7 +204,7 @@ public class DynamicArray {
      * @param index The position where the shifting to the right will occur
      * @param shiftPosition The integer to be shifting the array.
      */
-    public void shift(int index, int shiftPosition){
+    private void shift(int index, int add){
         //Capture the length of the container before re-creating it.
         int capacity = this.container.length;
         //Create a temporary array.
@@ -142,16 +212,32 @@ public class DynamicArray {
         //Recreate the container.
         this.container = new Object[capacity];
         
-        for (int i = 1; i < this.container.length; i++ ){
-            //Adds all the objects from the left of the index into the container.
-            this.container[i] = tempArray[i];
+        for (int i = 0; i < this.container.length-1; i++ ){
             
-            if (i >= index){
+            if (i == index){
                 //Leaves the position at index empty, and adds the rest on the right of it.
-                this.container[i+shiftPosition] = tempArray[i];                
-            }         
+                //this.container[i+shiftPosition] = tempArray[i]; 
+                if (add == 1){
+                    this.container[i+1] = tempArray[i];
+                    i++;
+                    log.debug("Container: " + i + ": " + this.container[3]);
+                    
+                }
+                log.debug("SHIFTING: " + i + ": " + this.container[i]);
+            } else {
+                //Adds all the objects from the left of the index into the container.
+                this.container[i] = tempArray[i];
+                log.debug("Container: " + i + ": " + this.container[i]);
+            }       
             
         }                
     } // End of shift
+
     
+    
+    
+    @Override
+    public String toString() {
+        return "DynamicArray{" + "container=" + container + ", size=" + size + '}';
+    }
 } // End of DynamicArray
--- src/test/java/DynamicArrayTester.java
+++ src/test/java/DynamicArrayTester.java
@@ -1,6 +1,9 @@
 
 import com.rimidev.array.DynamicArray;
 import org.junit.*;
+import static org.junit.Assert.fail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This is the class that will test the workings of the DyanmicArray class.
@@ -12,19 +15,150 @@ public class DynamicArrayTester {
     //Create the dynamic array
     DynamicArray array = new DynamicArray();
     
+    //Logger for debugging.
+    private final Logger log = LoggerFactory.getLogger(
+            this.getClass().getName());
+
     
+    
+    //WORKS
+    @Test
+    public void testAddAtEndOfTheArray(){      
+        String test = "Testing";
+        
+        array.add(test);
+     
+        Assert.assertEquals("The object is in the array", array.get(0), test);
+      
+    }
     
     @Test
-    public void testAddAtEndOfTheArray(){
+    public void testAddAtTheEndOfTheArrayWhenFullyOccupied(){
+        for (int i = 0; i < 10; i++){
+            String test = "Testing" + i;
+            array.add(i,test);
+        }
+        
+        log.debug("arraySize1: " + array.size());
+        
+        array.add("testing11");
+        
+        log.debug("arraySize2: " + array.size());
+        
+        Assert.assertEquals("The size of the object is 11", 11, array.size());
+        
+    }
+    //WORKS
+    @Test
+    public void testAddAtIndex(){
+        String test = "Testing";
+        
+        array.add(0,test);
+        
+        log.debug("Array size: " + array.size());
+        
+        Assert.assertEquals("The object is in the array at the correct index",
+                array.get(0), test);
+    }
+    //WORKS
+    @Test
+    public void testAddAtIndexWhenOccupiedIndex(){
+        String test = "FirstAdd";
+        
+        array.add(0,test);
+        
+        String test2 = "SecondAdd";
+        
+        array.add(0,test2);
+        
+        log.debug("Array at 0: " + array.get(0));
+        
+        Assert.assertEquals("The object is in the array at index",
+                array.get(0), test2);
+    }
+    
+    @Test(timeout = 1000, expected = IndexOutOfBoundsException.class)
+    public void testAddAtInvalidIndex() {
+        String test = "Testing";
+        
+        array.add(3,test);
+        
+        fail("Cannot set a value at index that is greater than the size.");
+    }
+    //WORKS
+    @Test
+    public void testGet(){
         String test = "Testing";
         
         array.add(test);
         
+        Object x = array.get(0);
         
+        Assert.assertEquals("The object is the same", x, test);
+    }
+    
+    //WORKS
+    //Only works if '==' and not '.equals'??
+    @Test
+    public void testIndexOf(){
+        String test = "Testing";
+        
+        array.add(0,test);
+        
+        int index = array.indexOf(test);
+        
+        Assert.assertEquals("The object has been found at the correct index",
+                0, index);
+    }
+    //WORKS
+    @Test
+    public void testRemoveAtIndex(){
+        String test = "Testing";
+        
+        array.add(0,test);
+        
+        array.remove(0);
+        
+        Object x = null;
+        
+        Assert.assertEquals("The object at the index has been removed", x, array.get(0));
+    }
+    //WORKS
+    @Test
+    public void testRemoveAtTheEnd(){
+        String test = "Testing";
+        
+        array.add(test);
+        
+        array.remove();
+        
+        Object x = null;
+        
+        Assert.assertEquals("The object at the end has been removed", x, array.get(0));
+    }
+    //RESTRICTION WORKS
+    @Test(timeout = 1000, expected = IndexOutOfBoundsException.class)
+    public void testSetAtInvalidIndexPosition(){
+        String test = "Testing";
+        String switched = "Switched";
+        
+        array.add(3,test);
+        array.set(3,switched);
+        
+        fail("Cannot set a value at index that is greater than the size.");
         
     }
     
-    
-    
+    @Test
+    public void testSet(){
+        String test = "Testing";
+        String switched = "Switched";
+        
+        array.add(test);
+        array.set(0,switched);
+        
+        Assert.assertEquals("The object has been switched correctly", 
+                switched, array.get(0));
+    }
     
 }
